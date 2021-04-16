680cbb62f698464558a886bd8e498a006759d2fc
Gilles Henrard <gilles.henrard@hotmail.com>
2021-04-16 21:33:39 +0200

    reorganise octaves + add notes prototypes

--- pitches.h
+++ pitches.h
@@ -1,10 +1,17 @@
 #ifndef INCLUDE_PITCHES_H
 #define INCLUDE_PITCHES_H
 
+#define TYP_A 0
+#define TYP_B 2
+#define TYP_C 3
+#define TYP_D 5
+#define TYP_E 7
+#define TYP_F 8
+#define TYP_G 10
+
 #define NOTE_A0   0
 #define NOTE_As0  1
 #define NOTE_B0   2
-
 #define NOTE_C1   3
 #define NOTE_Cs1  4
 #define NOTE_D1   5
@@ -14,10 +21,10 @@
 #define NOTE_Fs1  9
 #define NOTE_G1   10
 #define NOTE_Gs1  11
+
 #define NOTE_A1   12
 #define NOTE_As1  13
 #define NOTE_B1   14
-
 #define NOTE_C2   15
 #define NOTE_Cs2  16
 #define NOTE_D2   17
@@ -27,10 +34,10 @@
 #define NOTE_Fs2  21
 #define NOTE_G2   22
 #define NOTE_Gs2  23
+
 #define NOTE_A2   24
 #define NOTE_As2  25
 #define NOTE_B2   26
-
 #define NOTE_C3   27
 #define NOTE_Cs3  28
 #define NOTE_D3   29
@@ -40,10 +47,10 @@
 #define NOTE_Fs3  33
 #define NOTE_G3   34
 #define NOTE_Gs3  35
+
 #define NOTE_A3   36
 #define NOTE_As3  37
 #define NOTE_B3   38
-
 #define NOTE_C4   39
 #define NOTE_Cs4  40
 #define NOTE_D4   41
@@ -53,10 +60,10 @@
 #define NOTE_Fs4  45
 #define NOTE_G4   46
 #define NOTE_Gs4  47
+
 #define NOTE_A4   48
 #define NOTE_As4  49
 #define NOTE_B4   50
-
 #define NOTE_C5   51
 #define NOTE_Cs5  52
 #define NOTE_D5   53
@@ -66,10 +73,10 @@
 #define NOTE_Fs5  57
 #define NOTE_G5   58
 #define NOTE_Gs5  59
+
 #define NOTE_A5   60
 #define NOTE_As5  61
 #define NOTE_B5   62
-
 #define NOTE_C6   63
 #define NOTE_Cs6  64
 #define NOTE_D6   65
@@ -79,10 +86,10 @@
 #define NOTE_Fs6  69
 #define NOTE_G6   70
 #define NOTE_Gs6  71
+
 #define NOTE_A6   72
 #define NOTE_As6  73
 #define NOTE_B6   74
-
 #define NOTE_C7   75
 #define NOTE_Cs7  76
 #define NOTE_D7   77
@@ -92,10 +99,10 @@
 #define NOTE_Fs7  81
 #define NOTE_G7   82
 #define NOTE_Gs7  83
+
 #define NOTE_A7   84
 #define NOTE_As7  85
 #define NOTE_B7   86
-
 #define NOTE_C8   87
 #define NOTE_Cs8  88
 #define NOTE_D8   89
@@ -105,6 +112,7 @@
 #define NOTE_Fs8  93
 #define NOTE_G8   94
 #define NOTE_Gs8  95
+
 #define NOTE_A8   96
 #define NOTE_As8  97
 #define NOTE_B8   98
